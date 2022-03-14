f4babe37f8cce1820cd11e3f5134915321a435c8
Panda0078 <zhongjie.wang@kellogg.northwestern.edu>
2022-03-14 16:23:09 +0000

    update1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ email: params["email"] })
+      if @user
+        if BCrypt::Password.new(@user.password) == params["password"]
+          session[:user_id] = @user.id
+          flash[:notice] = "Welcome you are now logged in, #{@user.username}!"
+          redirect_to "/places"
+        else
+          flash[:notice] = "Incorrect password."
+          redirect_to "/sessions/new"
+        end
+        
+      else 
+          flash[:notice] = "Email is not registered, Please sign up"
+          redirect_to "/sessions/new"
+      end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash[:notice] = "Logged out"
+    redirect_to "/sessions/new"
   end
 end
   
