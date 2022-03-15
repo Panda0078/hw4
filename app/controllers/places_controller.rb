dfb16de1da3636ffbd635ffb53c0245748715817
Panda0078 <zhongjie.wang@kellogg.northwestern.edu>
2022-03-15 03:46:05 +0000

    Finalvv

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,10 +13,8 @@ class PlacesController < ApplicationController
   end
 
   def create
-    if @current_user
-      @place = Place.new(params["place"])
-      @post.user_id = @current_user.id
-      @place.save
+    @place = Place.new(params["place"])
+    @place.save
     redirect_to "/places"
   end
 
